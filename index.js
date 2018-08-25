6b28621bb92f8b2e236392c531e143e4cde45161
John Sanders <jws121295@gmail.com>
2018-08-25 18:55:18 -0400

    colors and orientation tracking

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let originY = window.innerHeight / 2
 let originRotate = 0
 let targetX = originX
 let targetY = originY
-const spawnFrameLimit = 12
+const spawnFrameLimit = 6
 let spawnFrameCount = 0
 
 const two = new Two({
@@ -32,10 +32,14 @@ window.addEventListener('resize', function () {
   two.trigger('resize')
 })
 
+let red = 255
+let green = 255
+let blue = 255
+
 function makeRectangle () {
   const r = two.makeRectangle(originX, originY, 0, 0)
-  r.stroke = 'rgb(255,255,255)'
-  r.linewidth = '2'
+  r.stroke = `rgb(${red}, ${green}, ${blue})`
+  r.linewidth = '3'
   r.fill = 'rgb(0,0,0)'
   r.rotation = originRotate
 
@@ -43,6 +47,7 @@ function makeRectangle () {
     const growthRate = 5
     r.width += growthRate
     r.height += growthRate
+    r.stroke = `rgb(${red}, ${green}, ${blue})`
 
     const diag = Math.sqrt(r.width*r.width)/4
     const left = r.translation.x - diag
@@ -81,6 +86,71 @@ two.bind('update', function updateOrigin () {
     spawnFrameCount = 0
   }
 })
+
+function scaleRange (opts, val) {
+  const targetRange = opts.maxTarget - opts.minTarget // 90
+  const origRange = opts.maxOrig - opts.minOrig // 180
+  return opts.minTarget + ((val - opts.minOrig) / origRange) * targetRange;
+}
+
+function clamp (magnitude, value) {
+  if (value < -magnitude) return -magnitude
+  if (value > magnitude) return magnitude
+  return value
+}
+
+const xout = document.querySelector('#x');
+const yout = document.querySelector('#y');
+const zout = document.querySelector('#z');
+
+// orientation tracking
+const xMaxMag = 45
+const yMaxMag = 45
+window.addEventListener('deviceorientation', function (event) {
+  let xRot = event.beta
+  let yRot = event.gamma
+  let zRot = event.alpha
+
+  const _90to45 = {
+    minOrig: -90,
+    maxOrig: 90,
+    minTarget: -45,
+    maxTarget: 45
+  }
+
+  xRot = clamp(45, xRot)
+  yRot = clamp(45, yRot)
+
+  xout.innerHTML = xRot
+  yout.innerHTML = yRot
+
+  targetY = scaleRange({
+    minOrig: -45,
+    maxOrig: 45,
+    minTarget: two.height,
+    maxTarget: 0
+  }, xRot)
+
+  targetX = scaleRange({
+    minOrig: -45,
+    maxOrig: 45,
+    minTarget: two.width,
+    maxTarget: 0
+  }, yRot)
+
+  ///////
+
+  zRot -= 180
+  zRot = Math.abs(zRot)
+  blue = green = scaleRange({
+    minOrig: 0,
+    maxOrig: 180,
+    minTarget: 0,
+    maxTarget: 255
+  }, zRot)
+  zout.innerHTML = zRot
+  yout.innerHTML = blue
+}, true)
 
 // kickoff
 two.renderer.domElement = document.querySelector('canvas')
